05584cf28dfffde3d99f255c082b047772e9e116
Justin Danglade <just.danglade@gmail.com>
2020-06-05 10:03:17 -0400

    Now using OpenWeatherMap API

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const HTTPS = require("https");
 const EXPRESS = require("express");
+const BODY_PARSER = require("body-parser");
+
 const APP = EXPRESS();
 var port = 3000;
 
+APP.use(BODY_PARSER.urlencoded({extended:true}));
 
 APP.get("/", function(req, res) {
-  const url = "HTTPS://api.openweathermap.org/data/2.5/weather?q=Miami&appid=30fe495dae8b4c3f453f0cb82c846a92&units=imperial";
+  res.sendFile(__dirname + "/index.html")
+});
+
+APP.post("/", function(req, res) {
+  const city = req.body.cityName
+  var apiKey = "30fe495dae8b4c3f453f0cb82c846a92";
+  var units = "imperial";
+  const url = "HTTPS://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=" + units;
 
   HTTPS.get(url, function(response) {
     console.log(response.statusCode);
@@ -14,10 +24,21 @@ APP.get("/", function(req, res) {
       const WEATHER = JSON.parse(data);
       const TEMP = WEATHER.main.temp;
       const DESCRIPTION = WEATHER.weather[0].description;
-      res.send("<h1>" + DESCRIPTION + "</h1><h2>Temperature: " + TEMP + "</h2>");
+      const ICON = WEATHER.weather[0].icon;
+      const ICON_URL = "http://openweathermap.org/img/wn/" + ICON + "@2x.png"
+
+
+      res.write("<h1>Temperature: " + TEMP + "</h1>");
+      res.write("<p>The weather in " + city + " is currently " + DESCRIPTION + "</p>")
+      res.write("<img src=" + ICON_URL + ">");
+      res.send();
     });
+
+
   });
 });
+
+
 
 APP.listen(port, function() {
   console.log("Listening on port " + port + ".");
